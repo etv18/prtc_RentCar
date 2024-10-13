d2fd27fda8eb6dd34cdc9dec509b5dcc858da90c
Emmanuel Tavarez <tavareze2003@gmail.com>
2024-10-12 20:10:49 -0400

    Se creo la vista index y edit, pero edit no redirige a index

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ClienteController;
+use GuzzleHttp\Client;
 use Illuminate\Support\Facades\Route;
+use App\Models\Cliente;
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,6 +18,12 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/clientes', [ClienteController::class, 'index'])->name('clientes.index');
+    Route::get('/crear-cliente', [ClienteController::class, 'create'])->name('clientes.create');
+    Route::post('/clientes', [ClienteController::class, 'store'])->name('clientes.store');
+    Route::get('/editar-cliente/{cliente}', [ClienteController::class, 'edit'])->name('clientes.edit');
+    Route::put('/cliente/{cliente}', [ClienteController::class, 'update'])->name('clientes.update');
 });
 
 require __DIR__ . '/auth.php';
