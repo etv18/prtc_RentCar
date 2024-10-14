a2f65bd468c365024c01c3da5b503d5ebb8f853f
Emmanuel Tavarez <tavareze2003@gmail.com>
2024-10-13 22:21:52 -0400

    Model, Migracion y Controller de Detalle_Factura fueron creados

---
+++ app/Http/Controllers/DetalleFacturaController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Detalle_Factura;
+use Illuminate\Http\Request;
+
+class DetalleFacturaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Detalle_Factura $detalle_Factura)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Detalle_Factura $detalle_Factura)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Detalle_Factura $detalle_Factura)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Detalle_Factura $detalle_Factura)
+    {
+        //
+    }
+}
