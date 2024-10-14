5f7eba3e2c7742a7e2d143c5c9ba45d5c64b0971
Emmanuel Tavarez <tavareze2003@gmail.com>
2024-10-13 22:26:29 -0400

    mcr de Factura creados

--- database/migrations/2024_10_14_022004_create_detalle__facturas_table.php
+++ database/migrations/2024_10_14_022004_create_detalle__facturas_table.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models;
+
 
 return new class extends Migration
 {
@@ -13,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('detalle__facturas', function (Blueprint $table) {
             $table->id();
+            $table->integer()
             $table->timestamps();
         });
     }
